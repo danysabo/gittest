cf1eec32f0f4a79e3706d12ab63c34de2f01a507
danysabo <danysabo@gmail.com>
2012-10-18 15:17:11 +0200

    Main class added.

---
+++ src/com/dany/Main.java
@@ -0,0 +1,12 @@
+package com.dany;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, test.");
+    }
+
+}
