5b405a72d4ef1f6ce8acc9f9b60af7f56c3178cc
Boris Đemrovski <djboris88@gmail.com>
2017-06-20 21:41:28 +0200

    Solved Array-3 > MaxSpan exercise with HashMap

---
+++ src/Max.java
@@ -0,0 +1,34 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Boris on 20.6.17.
+ */
+public class Max {
+    public int intMax(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+
+    public int maxSpan(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int y = i; y < nums.length; y++) {
+                if (nums[i] == nums[y]) {
+                    int sp = y - i + 1;
+                    if (m.get(nums[i]) == null || m.get(nums[i]) < sp) {
+                        m.put(nums[i], sp);
+                    }
+                }
+            }
+        }
+
+        Collection<Integer> values = m.values();
+        return Collections.max(values);
+
+    }
+}
---
+++ test/MaxTest.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Boris on 20.6.17.
+ */
+public class MaxTest {
+
+    private Max max = new Max();
+
+    @Test
+    public void testIntMax() {
+        Max max = new Max();
+        int m = max.intMax(1, 2, 3);
+        Assert.assertEquals(3, m);
+    }
+
+    @Test
+    public void testMaxSpan() {
+        Max max = new Max();
+        int[] span = {1, 2, 1, 1, 3};
+        int m = max.maxSpan(span);
+        Assert.assertEquals(4, m);
+    }
+
+}
